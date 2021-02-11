a757d14de5f66a4ed0e53bc92484a6dc00f668a5
VolodymyrVoloshyn02 <volodimirvoloshin02@gmail.com>
2021-02-11 21:39:39 +0200

    red button in spinner

--- app/src/main/java/com/example/myproject/CostChoseActivity.kt
+++ app/src/main/java/com/example/myproject/CostChoseActivity.kt
@@ -1,12 +1,11 @@
 package com.example.myproject
 import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Toast
+import android.view.ViewGroup
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myproject.db.DataBase
@@ -14,6 +13,8 @@ import com.example.myproject.db.DbManager
 import kotlinx.android.synthetic.main.activity_cost_chose.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
+
 
 fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
     val formatter = SimpleDateFormat(format, locale)
@@ -25,6 +26,7 @@ fun getCurrentDateTime(): Date {
 
 class CostChoseActivity : AppCompatActivity() {
     private var currentChose = 0
+    private var stringsInSpinner: ArrayList<String> = arrayListOf("")
     private val dbManager = DbManager.getInstance(this)
     private val arrOfImages = arrayOf(R.drawable.credyt, R.drawable.dishes,
         R.drawable.activities, R.drawable.transport, R.drawable.utiltties)
@@ -49,6 +51,22 @@ class CostChoseActivity : AppCompatActivity() {
                 changeImage()
             }
         }
+
+        stringsInSpinner = DbManager.getInstance(this).readColumn(DataBase.TABLE_OUTCOME_CATEGORY_NAME, DataBase.COLUMN_OUTCOME_CATEGORY_NAME)
+
+
+        outcomeSpinner.adapter = object : ArrayAdapter<String> (this, R.layout.style_spinner, stringsInSpinner){
+            override fun getDropDownView(
+                position: Int,
+                convertView: View?,
+                parent: ViewGroup
+            ): View {
+                val tv = super.getDropDownView(position, convertView, parent) as TextView
+                if (position == stringsInSpinner.size - 1)
+                    tv.setBackgroundColor(Color.RED);
+                return tv
+            }
+        }
     }
 
     private fun changeImage(){
@@ -58,11 +76,6 @@ class CostChoseActivity : AppCompatActivity() {
             outcomeImageView.setImageResource(R.drawable.outcome)
     }
 
-    private fun saveOutcome(idOfCategory: Int, sum: Float){
-        val outcomeEvent = OutcomeEvent(idOfCategory, sum, getCurrentDateTime().toString("yyyy/MM/dd"))
-        dbManager.insertToDb(outcomeEvent, DataBase.TABLE_OUTCOME_NAME)
-        currentBalance -= sum
-    }
 
     private fun callDialog(){
         val promptsView = LayoutInflater.from(this).inflate(R.layout.prompt, null);
@@ -85,8 +98,14 @@ class CostChoseActivity : AppCompatActivity() {
     }
 
     private fun updateSpinner(){
-        val stringsInSpinner = DbManager.getInstance(this).readColumn(DataBase.TABLE_OUTCOME_CATEGORY_NAME, DataBase.COLUMN_OUTCOME_CATEGORY_NAME)
-        outcomeSpinner.adapter = ArrayAdapter<String>(this, R.layout.style_spinner, stringsInSpinner)
+        stringsInSpinner = DbManager.getInstance(this).readColumn(DataBase.TABLE_OUTCOME_CATEGORY_NAME, DataBase.COLUMN_OUTCOME_CATEGORY_NAME)
+    }
+
+
+    private fun saveOutcome(idOfCategory: Int, sum: Float){
+        val outcomeEvent = OutcomeEvent(idOfCategory, sum, getCurrentDateTime().toString("yyyy/MM/dd"))
+        dbManager.insertToDb(outcomeEvent, DataBase.TABLE_OUTCOME_NAME)
+        currentBalance -= sum
     }
 
     fun safeSaveOutcome(view: View){

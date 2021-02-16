fbef2aca6e5541af6979131b27147fc324e7e906
SPavlo <warrioan@gmail.com>
2021-02-16 17:44:56 +0200

    add inCome, outCome fix

--- app/src/main/java/com/example/myproject/OutcomeHistory.kt
+++ app/src/main/java/com/example/myproject/OutcomeHistory.kt
@@ -5,28 +5,67 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.myproject.db.DataBase
 import com.example.myproject.db.DbManager
 import kotlinx.android.synthetic.main.activity_outcome_history.*
-
+import android.app.ActionBar
+import android.graphics.Color
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 
 class OutcomeHistory : AppCompatActivity() {
     private val myDbManager = DbManager.getInstance(this)
+    val ROWS = fillFields().size
+    val info = fillFields()
+    val COLUMNS = 3
+    val tableLayout by lazy { TableLayout(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_outcome_history)
-    }
-    override fun onResume() {
-        super.onResume()
-        fillFields()
+
+        val lp = TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        tableLayout.apply {
+            layoutParams = lp
+            isShrinkAllColumns = true
+        }
+        createTable(ROWS, COLUMNS)
     }
 
-    // function to calculate num of spaces for output//
-    private fun addSpases(s: String): Int {
-        val res = 19 + (19 - s.length)
-        return res
-    }
-    private fun fillFields() {
+    fun createTable(rows: Int, cols: Int) {
         var sum = 0.0
+        for (i in 0 until rows) {
+
+            val row = TableRow(this)
+            row.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+
+            for (j in 0 until cols) {
+                val layer = TextView(this)
+                layer.apply {
+                    layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                        TableRow.LayoutParams.WRAP_CONTENT)
+                    text = info[i][j] + " " + "\t" + "\n"
+                    textSize = 19F
+                }
+                layer.setTextColor(Color.parseColor("black"));
+
+                row.addView(layer)
+            }
+            tableLayout.addView(row)
+        }
+        linearLayout.addView(tableLayout)
+        val dataList =  myDbManager.readColumn(DataBase.TABLE_OUTCOME_NAME, DataBase.COLUMN_OUTCOME_VALUE)
+        for (item in dataList) {
+            sum += item.toFloat()
+        }
+        sumOutcome.text = sum.toString()
+    }
+
+    private fun fillFields() :ArrayList<ArrayList<String>> {
 
         val columns: Array<String> = arrayOf(
             "${DataBase.TABLE_OUTCOME_CATEGORY_NAME}.${DataBase.COLUMN_OUTCOME_CATEGORY_NAME}",
@@ -38,15 +77,6 @@ class OutcomeHistory : AppCompatActivity() {
         val info: ArrayList<ArrayList<String>> =
             myDbManager.tableOpenInformation(DataBase.TABLE_OUTCOME_NAME, columns, groupBy)
 
-        for (i in 0 until info.size) {
-            val temp = info[i][0].padEnd(addSpases(info[i][0]),' ') + info[i][1] + " \t\t" + info[i][2]
-            historyOutcomeData.append("${temp}\n\n")
-        }
-
-        val dataList =  myDbManager.readColumn(DataBase.TABLE_OUTCOME_NAME, DataBase.COLUMN_OUTCOME_VALUE)
-        for (item in dataList) {
-            sum += item.toFloat()
-        }
-        sumOutcome.text = sum.toString()
+       return info
     }
 }
